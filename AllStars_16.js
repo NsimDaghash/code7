caf545300e676b1d8e5ce5417dbf175a48bb3786
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2022-12-13 22:48:51 +0200

    All Star Code Challenge #16
    Create a function called noRepeat() that takes a string argument and returns a single letter string of the first not repeated character in the entire string.

---
+++ AllStars_16.js
@@ -0,0 +1,14 @@
+/*
+All Star Code Challenge #16
+
+Create a function called noRepeat() that takes a string argument and returns a single letter string of the first not repeated character in the entire string.
+
+noRepeat("aabbccdde") // => "e"
+noRepeat("wxyz") // => "w"
+noRepeat("testing") // => "e"
+Note: ONLY letters from the english alphabet will be used as input There will ALWAYS be at least one non-repeating letter in the input string
+*/
+
+function noRepeat(str) {  
+for(let i of str) if(str.split(i).length == 2) return i
+}
