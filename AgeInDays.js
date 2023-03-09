41d3fc8da920b43b92b5a7e37ce86c3156826101
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2023-03-09 23:48:32 +0200

    Age in days
    Did you ever want to know how many days old are you? Complete the function which returns your age in days. The birthday is given in the following order: year, month, day.

---
+++ AgeInDays.js
@@ -0,0 +1,17 @@
+/*
+Age in days
+
+Did you ever want to know how many days old are you? Complete the function which returns your age in days. The birthday is given in the following order: year, month, day.
+
+For example if today is 30 November 2015 then
+
+ageInDays(2015, 11, 1) returns "You are 29 days old"
+The birthday is expected to be in the past.
+
+*/
+function ageInDays(year, month, day) {
+  const birthday = new Date().getTime();
+  const date = new Date(year, month - 1, day).getTime();
+  const days = Math.floor(Math.abs(birthday - date) / (1000 * 3600 * 24));
+  return `You are ${days} days old`;
+}
