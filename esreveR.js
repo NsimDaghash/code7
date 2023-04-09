3beaad935fbbbff0b5444a8bb7732ab87de269f7
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2023-04-09 23:49:23 +0300

    esreveR
    Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
    (the dedicated builtin(s) functionalities are deactivated)

---
+++ esreveR.js
@@ -0,0 +1,15 @@
+/*
+esreveR 
+
+Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
+
+*/
+reverse = function (array) {
+  let tmpArr = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    tmpArr.push(array[i]);
+  }
+  return tmpArr;
+};
