c7c3352fc5f3f49c26765ec36f6a0e85658b6ad3
NsimDaghash <89819123+NsimDaghash@users.noreply.github.com>
2023-04-12 00:13:58 +0300

    Update StrongNumber.js
    now its working correctly

--- StrongNumber.js
+++ StrongNumber.js
@@ -1,22 +1,49 @@
+/*
+Definition
+Strong number is the number that the sum of the factorial of its digits is equal to number itself.
+
+For example, 145 is strong, since 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Task
+Given a number, Find if it is Strong or not and return either "STRONG!!!!" or "Not Strong !!".
+
+Notes
+Number passed is always Positive.
+Return the result as String
+Input >> Output Examples
+strong_num(1) ==> return "STRONG!!!!"
+Since, the sum of its digits' factorial (1) is equal to number itself, then its a Strong.
+
+strong_num(123) ==> return "Not Strong !!"
+Since the sum of its digits' factorial of 1! + 2! + 3! = 9 is not equal to number itself, then it's Not Strong .
+
+strong_num(2)  ==>  return "STRONG!!!!"
+Since the sum of its digits' factorial of 2! = 2 is equal to number itself, then its a Strong.
+
+strong_num(150) ==> return "Not Strong !!"
+Since the sum of its digits' factorial of 1! + 5! + 0! = 122 is not equal to number itself, Then it's Not Strong .
+*/
+
 function strong(n) {
-  let tmp = n + "";
-  tmp = tmp.split("");
+  let tmp = n;
   let t = 0,
-    tf = 0;
-  //   for (let i = 0; i < tmp.length; i++) {
-  //     t = t + +tmp[i];
-  //   }
-  let result = 1;
+    tf = 0,
+    result = 1;
+
   if (n < 3) {
     return "STRONG!!!!";
   }
-  for (let i = 0; i <= tmp.length; i++) {
-    for (let j = parseInt(tmp[i]); j > 0; j--) {
+  while (tmp >= 1) {
+    t = tmp % 10;
+    console.log("t :", t);
+    result = 1;
+    for (let j = t; j > 0; j--) {
       result = result * j;
       console.log(j);
     }
     tf = tf + result;
-    //  console.log(tf);
+    console.log("tf :", tf);
+    tmp = parseInt(tmp / 10);
   }
   if (n == tf) {
     return "STRONG!!!!";
@@ -25,5 +52,5 @@ function strong(n) {
   }
 }
 
-let n = 145;
+let n = 7;
 console.log(strong(n));
